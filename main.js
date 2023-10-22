e46c78c7cd2b2a4e4b5a612669397d987cc83131
JosiasRodrigues <120478571+JosiasRodrigues@users.noreply.github.com>
2023-10-22 18:18:49 -0300

    atualização de abas de senha e login

--- main.js
+++ main.js
@@ -23,16 +23,42 @@
     setCaret(input);
   };*/
 
-  const togglePassword = (button) => {
 
-    button.classList.toggle("showing");
-    const input = document.getElementById("password");
-    input.type = input.type === "password" ? "text" : "password";
-    input.focus();
+// Função para exibir uma mensagem de aviso
+function showErrorMessage(message) {
+  const errorMessage = document.createElement('div');
+  errorMessage.className = 'error-message';
+  errorMessage.textContent = message;
 
-  };
+  const form = document.querySelector('.form');
+  form.appendChild(errorMessage);
+
+  setTimeout(() => {
+    errorMessage.remove();
+  }, 3000);
+}
+
+// Função para verificar e lidar com o clique no botão "Sign Up"
+document.querySelector('.control[type="button"]').addEventListener('click', function () {
+  const textInput = document.querySelector('input[type="text"]');
+  const passwordInput = document.querySelector('input[type="password"]');
+
+  if (!textInput.value || !passwordInput.value) {
+    showErrorMessage('Por favor, preencha todos os campos.');
+  }
+});
+
+
+
+const togglePassword = (button) => {
+
+  button.classList.toggle("showing");
+  const input = document.getElementById("password");
+  input.type = input.type === "password" ? "text" : "password";
+  input.focus();
+
+};
 
 
 
 
-  
